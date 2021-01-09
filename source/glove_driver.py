48d7d95eefd1649912d0222f56d87aa62e007874
unknown <saurabh jain>
2021-01-09 14:00:33 +0800

    updated code to support training in chunks. If we train with whole dataset then memory error occurrs at the workstation machine.

--- source/glove_driver.py
+++ source/glove_driver.py
@@ -10,13 +10,30 @@ data_dir = os.path.join(root_dir, "data")
 model_dir = os.path.join(root_dir, "model")
 tag_table_file = os.path.join(data_dir, "paper_tag_table.csv")
 paper_table_file = os.path.join(data_dir, "paper_table.csv")
-glove_file = os.path.join(model_dir, "glove.6B.50d.txt")
+glove_file = os.path.join(model_dir, "glove.txt")
+output_glove_file = os.path.join(model_dir, "output_glove.txt")
 
 start_time = time.time()
 tag_set = set(pd.read_csv(tag_table_file)['tag_text'].dropna().tolist())
 abstract_list = pd.read_csv(paper_table_file)['abstract'].dropna().tolist()[:1]
 
-obj = PhraseGlove(pre_trained_glove_file=glove_file)
-obj.train(tag_set=tag_set, sentence_list=abstract_list)
-end_time = time.time()
-print(f"total time taken: {end_time - start_time}")
+obj = PhraseGlove(pre_trained_glove_file=glove_file, tag_set=tag_set, output_file=output_glove_file)
+
+start_index = 0
+num_sents = 10000
+if num_sents > len(abstract_list):
+    end_index = len(abstract_list)
+else:
+    end_index = start_index + num_sents
+
+while(end_index <= len(abstract_list)):
+    sent_list = abstract_list[start_index:end_index]
+    logging.info(f"sentences from index {start_index} to index {end_index}")
+    obj.train(sentence_list=sent_list)
+
+    start_index = end_index
+    end_index = start_index + num_sents
+
+logging.info("writing embeddings to output file...")
+obj.create_output_glove_file()
+print(f"total time taken: {time.time() - start_time}")
--- source/phrase_glove.py
+++ source/phrase_glove.py
@@ -13,13 +13,34 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 class PhraseGlove(object):
 
-    def __init__(self, pre_trained_glove_file, dimension=50, epochs=5):
-        self.dimension = 50
+    def __init__(self, pre_trained_glove_file, tag_set, output_file, dimension=50, epochs=5):
+        self.dimension = dimension
         self.epochs = epochs
+        self.output_file = output_file
 
         self.kp = KeywordProcessor()
         self.pre_trained_glove_file = pre_trained_glove_file
         self.pre_trained_glove_embeds = self.load_glove_file(pre_trained_glove_file)
+        self.create_kw_processor(tag_set)
+
+    def create_output_glove_file(self):
+        #writes new embeddings to file.
+        with open(self.output_file, 'x', newline='') as embed_file:
+            out_writer = csv.writer(embed_file, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
+            for key in self.pre_trained_glove_embeds.keys():
+                out_writer.writerow([key] + list(self.pre_trained_glove_embeds[key]))
+        logging.info("embeddings have been written to file: {self.output_file}")
+
+    def create_kw_processor(self, tag_set):
+        tag_phrase_dict = {}
+        for tag in tag_set:
+            index = tag.find('(')
+            if index > -1:
+                tag = tag[:index]
+            tag = tag.strip().lower()
+            tag_phrase_dict[tag.replace(' ', '_')] = [tag]
+
+        self.kp.add_keywords_from_dict(tag_phrase_dict)
 
     def load_glove_file(self, glove_file):
         """
@@ -38,39 +59,29 @@ class PhraseGlove(object):
                     for line in reader}
         return embed
 
-    def pre_process(self, tag_set, sentence_list):
+    def pre_process(self, sentence_list):
 
         """
             Pre process input sentences.
 
             Arguments:
-            tag_set:        set, set of keyword phrases
             sentence_list:  list, list of abstract of papers
 
             Returns:
             processed_sent_list:    list, list of processed sentences
         """
-        tag_phrase_dict = {}
-        for tag in tag_set:
-            tag = tag.strip().lower()
-            tag_phrase_dict[tag.replace(' ', '_')] = [tag]
-
-        self.kp.add_keywords_from_dict(tag_phrase_dict)
 
         logging.info('pre processing input sentences ')
         word_list = []
-        oov_word_set = set()
         for sent in sentence_list:
             tmp_sent = self.kp.replace_keywords(sent.strip().lower())
             for word in gensim.utils.tokenize(tmp_sent):
                 word_list.append(word)
-                if word not in self.pre_trained_glove_embeds.keys():
-                    oov_word_set.add(word)
         
-        logging.info(f"number of oov words {len(oov_word_set)}")
-        return word_list, list(oov_word_set)
+        logging.info(f"number of unique words {len(set(word_list))}")
+        return word_list
 
-    def train(self, tag_set, sentence_list):
+    def train(self, sentence_list):
             """
             Main function to call to train word2vec on additional dataset.
 
@@ -78,24 +89,26 @@ class PhraseGlove(object):
             tag_set:             set, set of keyword phrases
             sentence_list:       list, list of abstract of papers
             """
-            word_list, oov_word_list = self.pre_process(tag_set, sentence_list)
+            word_list = self.pre_process(sentence_list)
 
             logging.info("generating cooccurrence matrix...")
-            cv = CountVectorizer(ngram_range=(1,1), vocabulary=oov_word_list)
+            cv = CountVectorizer(ngram_range=(1,1))
             matrix = cv.fit_transform([' '.join(word_list)])
             cooccur_matrix = (matrix.T * matrix)
             cooccur_matrix.setdiag(0)
             cooccur_matrix = cooccur_matrix.toarray()
+            logging.info(f"total number of words in co-occurrence matrix: {len(cooccur_matrix)}")
+            logging.info(f"vocab of co-occurrence matrix: ")
+            logging.info(cv.vocabulary_)
             
             #starts training
             logging.info("starting training...")
-            mittens_model = Mittens(n=50, max_iter=1000)
-            new_embeddings = mittens_model.fit(cooccur_matrix, vocab=oov_word_list,
+            mittens_model = Mittens(n=self.dimension, max_iter=self.epochs)
+            new_embeddings = mittens_model.fit(cooccur_matrix,
                 initial_embedding_dict= self.pre_trained_glove_embeds)
-
-            logging.info("writing new embedding to file...")
-            #writes new embeddings to file.
-            with open(self.pre_trained_glove_file, 'a', newline='') as embed_file:
-                out_writer = csv.writer(embed_file, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
-                for i in range(len(oov_word_list)):
-                    out_writer.writerow([oov_word_list[i]] + list(new_embeddings[i]))
+            logging.info(f"total number of words in new_embeddings dictionary: {len(new_embeddings)}")
+            tmp_dict = cv.vocabulary_.copy()
+            for key in tmp_dict.keys():
+                tmp_dict[key] = new_embeddings[tmp_dict[key]]
+            logging.info(f"updating embeddings with new embeddings...")
+            self.pre_trained_glove_embeds.update(tmp_dict)
